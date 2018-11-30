4392134db84543eb58aa4ab9501550418ab72d8d
U-SOTON\pk1g17 <pk1g17@JFB59-R3229-M01.soton.ac.uk>
2018-11-30 15:24:35 +0000

    removed unncecessary comments

--- eval.c
+++ eval.c
@@ -5,41 +5,6 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
-long eval(mpc_ast_t* sentence) {
-	
-	This is a structure which we get after parsing the input. Might be helpful to understand what's going on in the function evaluating it
-	typedef struct mpc_ast_t {
-	char* tag;
-	char* contents;
-	mpc_state_t state;
-	int children_num;
-	struct mpc_ast_t** children;
-	} mpc_ast_t; 
-
-	//if the sentence is a number, return the number
-	if (strstr(sentence->tag, "number")) {
-		return atoi(sentence->contents); // The contents are always chars so we convert it to integer
-	}
-	//If it's not a number, we are looking for a operator which is always a second child of the expression(the first one is regular expression mark, opening parentheeses '('	)
-	char* operator = sentence->children[1]->contents;
-	//if there is - and onlt 1 numner return -number
-	if (!strcmp("-", operator) && !strstr(sentence->children[3]->tag, "expression")) {
-		return  -1 * atoi(sentence->children[2]->contents);
-	}
-	//the third child can be either number or another expression, so we recursively call eval on it
-
-	long sum = eval(sentence->children[2]);
-	//iterate through remaining n children starting from position 3 and combine everything
-	int i = 3;
-	while (strstr(sentence->children[i]->tag, "expression")) {
-		sum = eval_operator(sum, operator, eval(sentence->children[i]));
-		i++;
-	}
-	return sum;
-
-}
-*/
 
 //eval for lisp_value
 
@@ -69,29 +34,6 @@ lisp_value eval(mpc_ast_t* sentence) {
 
 }
 
-/*long eval_operator(long current_sum, char* op, long next_number) {
-	if (!strcmp("+", op))
-		return current_sum + next_number;
-	if (!strcmp("-", op))
-		return current_sum - next_number;
-	if (!strcmp("*", op))
-		return current_sum * next_number;
-	if (!strcmp("/", op))
-		return current_sum / next_number;
-	if (!strcmp("%", op))
-		return current_sum % next_number;
-	if (!strcmp("min", op))
-		return current_sum < next_number ? current_sum : next_number;
-	if (!strcmp("max", op))
-		return current_sum > next_number ? current_sum : next_number;
-	if (!strcmp("^", op))
-		return pow(current_sum, next_number);
-
-	return 0;
-
-} */
-
-//eval version able to handle lisp_value structure
 
 lisp_value eval_operator(lisp_value current_value, char* operator, lisp_value new_value) {
 	//if either of the values is a known error, return it
